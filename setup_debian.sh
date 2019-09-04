1d159d1e45a9646d0f8a62a35e9a448f7f30dc42
Joni Hasanen <joni.hasanen@pieceofcode.net>
2019-09-04 22:55:01 +0300

    Updates

--- setup_debian.sh
+++ setup_debian.sh
@@ -12,6 +12,10 @@ $sudo add-apt-repository \
    stable"
 $sudo apt-get update
 $sudo apt-get install -y ffmpeg \
+  gnome-tweak-tool \
+  libssl-dev \
+  libreadline-dev \
+  zlib1g-dev \
   ansible \
   git \
   p7zip \
