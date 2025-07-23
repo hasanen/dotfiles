c98a0be58332ed575e5465eddf8a3899f3ff063b
Joni Hasanen <joni@pieceofcode.com>
2025-07-23 21:07:39 +0300

    Update order of things

--- setup_debian.sh
+++ setup_debian.sh
@@ -4,6 +4,19 @@ if [ $1 ]; then
   sudo=sudo
 fi
 
+## 1Password
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | $sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | $sudo tee /etc/apt/sources.list.d/1password.list
+$sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | $sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+$sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | $sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
+## Visual Studio Code 
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+$sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" | $sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+
 
 $sudo apt-get update
 $sudo apt-get install -y ffmpeg \
@@ -25,7 +38,9 @@ $sudo apt-get install -y ffmpeg \
   build-essential \
   flatpak \
   syncthing \ 
-  xfce4-whiskermenu-plugin
+  xfce4-whiskermenu-plugin \
+  1password \
+  code 
 
 
 curl -sS https://starship.rs/install.sh | sh
@@ -35,13 +50,3 @@ curl -sS https://starship.rs/install.sh | sh
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub md.obsidian.Obsidian
 flatpak install flathub com.discordapp.Discord
-
-
-## 1Password
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | $sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | $sudo tee /etc/apt/sources.list.d/1password.list
-$sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | $sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-$sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | $sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-sudo apt update && sudo apt install 1password
