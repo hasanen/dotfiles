77d523cab05d45e4e46d2830c347ad49d5525126
Joni Hasanen <joni@pieceofcode.com>
2025-07-23 21:00:00 +0300

    Update setup for debian

--- setup_debian.sh
+++ setup_debian.sh
@@ -21,6 +21,27 @@ $sudo apt-get install -y ffmpeg \
   zsh \
   mosh \
   screenfetch \
-  vlc
+  vlc \
+  build-essential \
+  flatpak \
+  syncthing \ 
+  xfce4-whiskermenu-plugin
+
 
 curl -sS https://starship.rs/install.sh | sh
+
+
+## Flatpak 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub com.discordapp.Discord
+
+
+## 1Password
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | $sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | $sudo tee /etc/apt/sources.list.d/1password.list
+$sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | $sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+$sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | $sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+sudo apt update && sudo apt install 1password
